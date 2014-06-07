ce0117e065f538f66a80eeeb4940c0669c739125
SIMPUS GAMER <alfonso.calderon.chavez@gmail.com>
2014-06-07 11:47:21 -0500

    Disenio y avance en egresos

--- Parroquia/Parroquia/Egresos.Designer.cs
+++ Parroquia/Parroquia/Egresos.Designer.cs
@@ -38,7 +38,6 @@
             // 
             this.tabla.AllowUserToAddRows = false;
             this.tabla.AllowUserToOrderColumns = true;
-            this.tabla.AllowUserToResizeColumns = false;
             this.tabla.AllowUserToResizeRows = false;
             this.tabla.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
@@ -46,7 +45,8 @@
             this.tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.tabla.Location = new System.Drawing.Point(12, 12);
             this.tabla.Name = "tabla";
-            this.tabla.Size = new System.Drawing.Size(673, 220);
+            this.tabla.RowHeadersWidth = 10;
+            this.tabla.Size = new System.Drawing.Size(830, 355);
             this.tabla.TabIndex = 0;
             this.tabla.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.tabla_CellEndEdit);
             // 
@@ -54,9 +54,9 @@
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(519, 237);
+            this.button1.Location = new System.Drawing.Point(666, 372);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.Size = new System.Drawing.Size(85, 33);
             this.button1.TabIndex = 1;
             this.button1.Text = "GUARDAR";
             this.button1.UseVisualStyleBackColor = true;
@@ -65,9 +65,9 @@
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(610, 237);
+            this.button2.Location = new System.Drawing.Point(757, 372);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.Size = new System.Drawing.Size(85, 33);
             this.button2.TabIndex = 2;
             this.button2.Text = "CANCELAR";
             this.button2.UseVisualStyleBackColor = true;
@@ -76,17 +76,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 277);
+            this.ClientSize = new System.Drawing.Size(854, 412);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tabla);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Egresos";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Egresos";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.tabla)).EndInit();
             this.ResumeLayout(false);
 
